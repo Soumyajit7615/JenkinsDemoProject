c25de944a95fa76295a1f17fb0e3cf86aa4135dd
Soumyajit7615 <soumyajit7615@gmail.com>
2022-06-21 18:02:39 +0530

    add tutorial jenkins in index page

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinsDemoProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
